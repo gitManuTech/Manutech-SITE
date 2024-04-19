fe4f28ec20fa0a27146e88da49637296d60ac6da
Dante <truedevilhunterThe@gmail.com>
2024-04-19 13:28:38 -0300

    Adding directory for tests

---
+++ tests/MockDbConnect.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+
+final class MockDb extends TestCase
+{
+
+	public function verifyDatabaseConnection(): void
+	{
+	
+	}
+
+
+
+
+
+}
+
+?>
