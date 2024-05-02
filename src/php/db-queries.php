f85f0606416b670a71bb08ef6a2dedd2dd0a125d
Dante <truedevilhunterThe@gmail.com>
2024-05-02 13:37:05 -0300

    Moved 2 function, corrected a phrase, closed conn
    I've moved two "is..." functions into their separated file,
    also, I've closed properly the connection inside a function

--- src/php/db-queries.php
+++ src/php/db-queries.php
@@ -16,23 +16,6 @@ function get_credentials(string $sname, string $sclass, string $scourse)
 	return $query;
 }
 
-/**
- * @author	João Paulo Ferrari Sant'Ana 	joaopauloferrarisanta@gmail.com
- * @version	2.0.1				Will return an int that represents the student row
- * @since	1.0.0
- *
- * @return	int
- * */
-function is_student_registered(string $sname, string $sclass, string $scourse): int {
-	$student_data = get_credentials($sname, $sclass, $scourse);
-
-	if(!$student_data) {
-		return 0;
-	}
-
-	return $student_data->num_rows;
-}
-
 function insert_into_db(string $sname, string $room, string $course): void {
 	$mysql = connect_db();
 	$insert = "INSERT INTO student_tbl(student_name, student_classroom, student_course)
@@ -45,7 +28,7 @@ function insert_into_db(string $sname, string $room, string $course): void {
 
 /**
  * @author	João Paulo Ferrari Sant'Ana	joaopauloferrarisantana@gmail.com
- * @version	1.0.0				Will disable the student's account setting
+ * @version	1.0.0				Will disable the student's account, setting
  * 						the flag "student_active" to 0
  * @since	1.0.0
  * */
@@ -54,10 +37,7 @@ function disable_student_acc(string $sname): void {
 	$update	= "UPDATE student_tbl SET student_active = 0 WHERE student_name = \"".$sname."\"";
 
 	$mysql->query($update);
-}
-
-function is_info_in_db(string $student_info, string $db_data): bool {
-	return ($student_info === $db_data);
+	$mysql->close();
 }
 
 ?>
