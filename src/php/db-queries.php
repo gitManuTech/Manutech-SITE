f1aa286b72b63fd64cbd14ba2655c3dc58260b87
Dante <truedevilhunterThe@gmail.com>
2024-05-02 12:01:15 -0300

    Added error handling, documentation and a new func
    I've added an "if" to deal with the case of the student not being in the
    db
    Added documentation and a function to disable the student's account

--- src/php/db-queries.php
+++ src/php/db-queries.php
@@ -2,8 +2,8 @@
 
 require_once "db-connect.php";
 
-// https://www.php.net/manual/en/mysqli-result.fetch-assoc.php#refsect1-mysqli-result.fetch-assoc-returnvalues
-function get_credentials(string $sname, string $sclass, string $scourse): mysqli_result {
+function get_credentials(string $sname, string $sclass, string $scourse)
+: mysqli_result | bool {
 	$mysql	= connect_db();
 	$select	= "SELECT student_name, student_classroom, student_course 
 		FROM student_tbl WHERE
@@ -16,11 +16,9 @@ function get_credentials(string $sname, string $sclass, string $scourse): mysqli
 	return $query;
 }
 
-
-
 /**
  * @author	João Paulo Ferrari Sant'Ana 	joaopauloferrarisanta@gmail.com
- * @version	2.0.1				Will return an int that represents if the student row
+ * @version	2.0.1				Will return an int that represents the student row
  * @since	1.0.0
  *
  * @return	int
@@ -28,17 +26,34 @@ function get_credentials(string $sname, string $sclass, string $scourse): mysqli
 function is_student_registered(string $sname, string $sclass, string $scourse): int {
 	$student_data = get_credentials($sname, $sclass, $scourse);
 
+	if(!$student_data) {
+		return 0;
+	}
+
 	return $student_data->num_rows;
 }
 
 function insert_into_db(string $sname, string $room, string $course): void {
 	$mysql = connect_db();
 	$insert = "INSERT INTO student_tbl(student_name, student_classroom, student_course)
-	VALUES(\"".$sname."\", \"".$room."\", \"".$course."\")";
+		VALUES(\"".$sname."\", \"".$room."\", \"".$course."\")";
 
 	$query = $mysql->query($insert);
 
 	$mysql->close();
+}
+
+/**
+ * @author	João Paulo Ferrari Sant'Ana	joaopauloferrarisantana@gmail.com
+ * @version	1.0.0				Will disable the student's account setting
+ * 						the flag "student_active" to 0
+ * @since	1.0.0
+ * */
+function disable_student_acc(string $sname): void {
+	$mysql	= connect_db();
+	$update	= "UPDATE student_tbl SET student_active = 0 WHERE student_name = \"".$sname."\"";
+
+	$mysql->query($update);
 }
 
 function is_info_in_db(string $student_info, string $db_data): bool {
