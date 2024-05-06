a0db885de9a6213ca92008881f3800a069b03ab8
Dante <truedevilhunterThe@gmail.com>
2024-05-06 15:36:52 -0300

    Removing function and "if" statement
    The function was removed thanks to the fact that the verification
    will be made at the client

--- src/private/php/login-server.php
+++ src/private/php/login-server.php
@@ -5,24 +5,16 @@ session_start();
 require_once "db-queries.php";
 require_once "db-query-check.php";
 
-function is_post_empty(string $post_param): bool {
-	return empty($post_param);
-}
-
 $name	= $_POST["student-name"];
 $class	= $_POST["student-classroom"];
 $course = $_POST["student-course"];
-
-if(is_post_empty($name) || is_post_empty($class) || is_post_empty($course)) {
-	header("Location: ../html/login.html");
-}
 
 if(is_student_registered($name, $class, $course) == 0) {
 	echo "Estundate não está registrado.";
 	exit(0);
 }
 
-if(is_student_acc_disabled($name)) {
+if(is_student_acc_disabled($name) === true) {
 	echo "Conta do estudante foi desabilitada.";
 	exit(0);
 }
