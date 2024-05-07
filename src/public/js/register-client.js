2185bb79f683a6cd8f415f7411d7bfd2ec5d30be
Dante <truedevilhunterThe@gmail.com>
2024-05-07 16:04:53 -0300

    Added a way to the user login in the register-file

--- src/public/js/register-client.js
+++ src/public/js/register-client.js
@@ -1,12 +1,21 @@
-import { getStudentData } from "../../private/js/student-data.js"
-import { sendReq } from "../../private/js/send-server.js";
+import { getRawStudentData, getStudentData } from "../../private/js/student-data.js"
+import { createReq, mkReq } from "../../private/js/send-server.js";
+import { ELEMENTS_TO_SEND, SUBMIT_ERROR } from "../../private/js/enums.js";
 
 document.getElementById("student-form").addEventListener("submit", mkRegisterReq, true);
 
 function mkRegisterReq(e) {
-	const studentData = getStudentData();
+	const rawStudentData	= getRawStudentData();
+	const studentData	= getStudentData(rawStudentData);
+	const requestData	= createReq(rawStudentData, studentData, ELEMENTS_TO_SEND.ALL);
+	const requestPath	= "../../private/php/register-server.php";
 
-	sendReq(studentData, "../../private/php/register-server.php", "POST");
+	// I don't know if this is the best way to do it
+	if(requestData === SUBMIT_ERROR.PROBLEM_FIELDS) {
+		event.preventDefault();
+	} else {
+		mkReq(requestData, requestPath, true);
+	}
 
 	e.preventDefault();
 }
