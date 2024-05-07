7c365299a2d04ca5366d40690d70f2e1122f1225
Dante <truedevilhunterThe@gmail.com>
2024-05-07 16:21:52 -0300

    Added fetch for login-client
    Added a way to the user login in the login-client file

--- src/public/js/login-client.js
+++ src/public/js/login-client.js
@@ -1,12 +1,23 @@
-import { getStudentData } from "../../private/js/student-data.js";
-import { sendReq } from "../../private/js/send-server.js";
+import { getRawStudentData, getStudentData } from "../../private/js/student-data.js"
+import { createReq, mkReq } from "../../private/js/send-server.js";
+import { ELEMENTS_TO_SEND, SUBMIT_ERROR } from "../../private/js/enums.js";
 
 document.getElementById("student-form").addEventListener("submit", mkLoginReq, true);
 
 function mkLoginReq(e) {
-	const studentData = getStudentData();
+	const rawStudentData	= getRawStudentData();
+	const studentData	= getStudentData(rawStudentData);
 
-	sendReq(studentData, "../../private/php/login-server.php", "POST");
+	const requestData = createReq(rawStudentData, studentData, ELEMENTS_TO_SEND.ALL);
+	const requestPath = "../../private/php/login-server.php";
+
+	// I don't know if this is the best way to do it
+	if(requestData === SUBMIT_ERROR.PROBLEM_JSON) {
+		console.log(requestData);
+		console.log(studentData);
+	} else {
+		mkReq(requestData, requestPath, true);
+	}
 
 	e.preventDefault();
 }
