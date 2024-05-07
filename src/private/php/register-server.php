f0240887183a98bfe9091cb2326d0525dce80c34
Dante <truedevilhunterThe@gmail.com>
2024-05-07 16:02:00 -0300

    Added a way to the server get the client json

--- src/private/php/register-server.php
+++ src/private/php/register-server.php
@@ -4,11 +4,14 @@ session_start();
 
 require_once "db-queries.php";
 
+$incoming_json	= file_get_contents("php://input");
+$json_decoded	= json_decode($incoming_json, true);
+
 // TODO: Remove "query" and replace it with "prepare" statements
 
-$name		= $_POST["student-name"];
-$classroom	= $_POST["student-classroom"];
-$course		= $_POST["student-course"];
+$name		= $json_decoded["student-name"];
+$classroom	= $json_decoded["student-classroom"];
+$course		= $json_decoded["student-course"];
 
 if(is_student_registered($name, $classroom, $course) == 1) {
 	echo "Usuário já possui conta.";
