1266e3206a3462fa0470c299bfbc690069ff7554
Dante <truedevilhunterThe@gmail.com>
2024-05-10 13:15:44 -0300

    Cleared up a bit problem.php

--- src/private/php/problem.php
+++ src/private/php/problem.php
@@ -39,7 +39,7 @@ session_start();
 				<ol class="navbar">
 
 					<li><a href="./profile.php"><?php echo $_SESSION["sname"]; ?></a></li>
-					<li><a href="../html/">Home</a></li>
+					<li><a href="../../public/html/index.html">Home</a></li>
 
 				</ol>
 
@@ -55,9 +55,10 @@ session_start();
 
 					<div class="form-part problem-title">
 
-						<!-- TODO: Make the input better: filter stuff with JS... -->
-						<label for="problem-title">Resumo do problema</label>
-						<input type="text" id="problem-title"  class="complain-title" name="problem-title" placeholder="Titulo do problema" maxlength="32" autofocus required>
+						<label for="problem-title">Titulo do problema</label>
+						<input type="text"
+						id="problem-title" class="complain-title"
+						name="problem-title" placeholder="Titulo do problema" required>
 
 					</div>
 
@@ -65,7 +66,9 @@ session_start();
 					<div class="form-part problem-block">
 
 						<label for="problem-block">Identifique o bloco</label>
-						<input type="text" id="problem-block" class="complain-location" name="problem-block" placeholder="B1" maxlength="2" pattern="[A-Z]{1,1}\d{1,1}" required>
+						<input type="text"
+						id="problem-block" class="complain-location"
+						name="problem-block" placeholder="B1" required>
 
 					</div>
 
@@ -77,14 +80,12 @@ session_start();
 
 						<div class="problem-area">
 
-							<!-- TODO: Verify <textarea>'s length on Javascript, later down the line  -->
 							<label for="problem-description">Descreva o problema</label>
 
-							<!-- TODO: We might need to resize it later down -->
 							<textarea
 							id="problem-description" class="complain-description"
-							name="problem-descriptor" placeholder="Dê uma descrição detalhada ao problema"
-							maxlength="255" onkeydown="enableBtn()"></textarea>
+							name="problem-descriptor"
+							placeholder="Dê uma descrição detalhada ao problema"></textarea>
 
 						</div>
 
@@ -108,24 +109,8 @@ session_start();
 
 		</footer>
 
-		<script type="text/js" src="../js/problem-found-client.js"></script>
-
-		<script>
-
-			function enableBtn() {
-				const problemArea	= document.getElementById("problem-description");
-				const studentProblemLen	= problemArea.value.length;
-				const minLength		= parseInt(problemArea.maxLength * (15 / 100));
-				const submitBtn		= document.getElementById("submit-btn");
-
-				if(studentProblemLen >= minLength) {
-					submitBtn.disabled = false;
-				} else {
-					submitBtn.disabled = true;
-				}
-			}
-
-		</script>
+		<script type="text/js" src="../../public/js/problem-found-client.js"></script>
+		<script type="text/javascript" src="../../public/js/post-validator.js"></script>
 
 	</body>
 
