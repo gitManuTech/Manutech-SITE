c31ec0b2358bbbe72611864cd8044a2f68d9c440
Dante <truedevilhunterThe@gmail.com>
2024-05-14 13:32:16 -0300

    Added page to see others student's posts

---
+++ src/private/php/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="eng">
+
+	<head>
+
+		<title>Handyman - Home</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Home page for other posts">
+
+		<link rel="stylesheet" type="text/css" href="../../public/css/general.css">
+
+	</head>
+
+	<body>
+
+		<header class="header"></header>
+
+		<main class="main"></main>
+
+		<footer class="footer">
+
+			<p>Handyman group INC. &copy;2024</p>
+
+		</footer>
+
+	</body>
+
+<html>
