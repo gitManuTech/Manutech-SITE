e7426be57edafd6dd226174ce1d31f9116d09fda
Dante <truedevilhunterThe@gmail.com>
2024-05-15 11:30:05 -0300

    Added new function, added heredocs (finally)
    And changed the LIMIT clause limit's to 10

--- src/private/php/db-student-queries.php
+++ src/private/php/db-student-queries.php
@@ -3,28 +3,32 @@
 require_once "db-connect.php";
 
 function gen_student_page_post(string $title, string $desc, string $block): void {
-echo "
-<div class='post-header-container'>
 
-	<h2 class='post-header'>
-		<a href='student-post.php?ptitle=".$title."&pblock=".$block."&pdesc=".$desc."'>".$title."</a>
+echo <<<END
+
+<div class="post-header-container">
+
+	<h2 class="post-header">
+		<a href="student-post.php?ptitle={$title}&pblock={$block}&pdesc={$desc}">{$title}</a>
 	</h2>
 
 </div>
 
-<div class='post-desc'>
+<div class="post-desc">
 
-	<p class='post-text'>".$desc."</p>
+	<p class="post-text">{$desc}</p>
 
 </div>
-";
+
+END;
+
 }
 
 function select_posts(int $uid): mysqli_result {
 	$mysql = connect_db();
 	$posts = "SELECT
 		problem_title Title, problem_desc DSC, problem_block Block
-		FROM problem_tbl WHERE problem_tbl.student_id =".intval($uid)." LIMIT 5";
+		FROM problem_tbl WHERE problem_tbl.student_id =".intval($uid)." LIMIT 10";
 
 	$result = $mysql->query($posts);
 
@@ -42,4 +46,20 @@ function display_posts(int $uid): void {
 		gen_student_page_post($post_data["Title"], $post_data["DSC"], $post_data["Block"]);
 	}
 }
+
+function send_problem_data(string $ptitle, string $pblock, string $pdesc, int $uid): void {
+	$mysql		= connect_db();
+	$complain	= <<<END
+
+		INSERT INTO problem_tbl(
+			problem_title, problem_block, problem_desc, student_id
+		)
+		VALUES( "{$ptitle}", "{$pblock}", "{$pdesc}", "{$uid}" )
+
+	END;
+
+	$mysql->query($complain);
+	$mysql->close();
+}
+
 ?>
