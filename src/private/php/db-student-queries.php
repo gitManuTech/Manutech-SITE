71400ed23bc465a316417a6921319106a9dc3da1
Dante <truedevilhunterThe@gmail.com>
2024-05-16 16:11:29 -0300

    Moved a function to a file, added 2 functions
    And changed 1 func name to something descriptive

--- src/private/php/db-student-queries.php
+++ src/private/php/db-student-queries.php
@@ -1,33 +1,29 @@
 <?php
 
 require_once "db-connect.php";
+require_once "db-data-format.php";
 
-function gen_student_page_post(string $title, string $desc, string $block): void {
+// TODO: use namespaces
 
-echo <<<END
+function select_all_student_posts(): mysqli_result {
+	$mysql = connect_db();
 
-<div class="post-header-container">
+	// will search table "Problem" and will query for students that made posts
+	// I think that... we can optimise this query
+	$select_all_posts = "SELECT Problem.problem_title Title, Problem.problem_desc DSC,
+		Problem.problem_block Block, Student.student_name Author
+		FROM problem_tbl Problem
+		INNER JOIN student_tbl Student ON Problem.student_id = Student.student_id;";
 
-	<h2 class="post-header">
-		<a href="student-post.php?ptitle={$title}&pblock={$block}&pdesc={$desc}">{$title}</a>
-	</h2>
+	$data = $mysql->query($select_all_posts);
 
-</div>
-
-<div class="post-desc">
-
-	<p class="post-text">{$desc}</p>
-
-</div>
-
-END;
-
+	return $data;
 }
+
 
 function select_posts(int $uid): mysqli_result {
 	$mysql = connect_db();
-	$posts = "SELECT
-		problem_title Title, problem_desc DSC, problem_block Block
+	$posts = "SELECT problem_title Title, problem_desc DSC, problem_block Block
 		FROM problem_tbl WHERE problem_tbl.student_id =".intval($uid)." LIMIT 10";
 
 	$result = $mysql->query($posts);
@@ -39,23 +35,30 @@ function select_posts(int $uid): mysqli_result {
 	return $result;
 }
 
+// TODO: combine this with display_posts() function
+function display_all_posts(): void {
+	$posts = select_all_student_posts();
+
+	while($post_data = $posts->fetch_assoc()) {
+		generate_post_home($post_data["Title"], $post_data["DSC"], $post_data["Block"], $post_data["Author"]);
+	}
+}
+
 function display_posts(int $uid): void {
 	$posts = select_posts($uid);
 
 	while($post_data = $posts->fetch_assoc()) {
-		gen_student_page_post($post_data["Title"], $post_data["DSC"], $post_data["Block"]);
+		generate_post_profile($post_data["Title"], $post_data["DSC"], $post_data["Block"]);
 	}
 }
 
 function send_problem_data(string $ptitle, string $pblock, string $pdesc, int $uid): void {
 	$mysql		= connect_db();
 	$complain	= <<<END
-
 		INSERT INTO problem_tbl(
 			problem_title, problem_block, problem_desc, student_id
 		)
 		VALUES( "{$ptitle}", "{$pblock}", "{$pdesc}", "{$uid}" )
-
 	END;
 
 	$mysql->query($complain);
