e363c29fdffb55d877a44fb295a0e1203e696470
Dante <truedevilhunterThe@gmail.com>
2024-05-17 14:14:12 -0300

    Removed the same check for the 4th time
    And fixed path
    (I hope that this fix it finally)

--- src/private/php/register-server.php
+++ src/private/php/register-server.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once "db-queries.php";
+require_once "Database/db-queries.php";
 // require_once "json.php";
 
 // $student_data = get_json();
@@ -11,11 +11,6 @@ $name		= $_POST["student-name"];
 $classroom	= $_POST["student-classroom"];
 $course		= $_POST["student-course"];
 
-if(is_student_acc_disabled($name) == true) {
-	echo "Conta do estudante foi desabilitada";
-	exit(0);
-}
-
 if(is_student_registered($name, $classroom, $course) == 1) {
 	echo "Usuário já possui conta.";
 	exit(1);
@@ -23,9 +18,12 @@ if(is_student_registered($name, $classroom, $course) == 1) {
 
 insert_into_db($name, $classroom, $course);
 
-$_SESSION["sname"]	= $name;
-$_SESSION["sroom"]	= $classroom;
-$_SESSION["scourse"]	= $course;
+$student = get_credentials($name, $classroom, $course)->fetch_row();
+
+$_SESSION["uid"]	= $student[0];
+$_SESSION["sname"]	= $student[1];
+$_SESSION["sroom"]	= $student[2];
+$_SESSION["scourse"]	= $student[3];
 
 header("Location: ./profile.php");
 
