187d329a5ad0a0ffd1794a86e1ca3c6aaff2a8f7
Dante <truedevilhunterThe@gmail.com>
2024-05-19 18:35:04 -0300

    Essentially, removed a js file and remaked another
    For what I've done it in post-validator, chk-student-data has become
    unnecessary

--- src/private/js/post-validator.js
+++ src/private/js/post-validator.js
@@ -1,49 +1,18 @@
-import { enableOrDisableBtn } from "../../private/js/chk-student-data.js";
-import { calcMinFieldLen } from "../../private/math.js";
+export function isTitleOverLimit(titleLength) {
+	const titleMaxLength = 32;
 
-document.getElementById("problem-title").addEventListener("keydown", verifyTitleLength);
-document.getElementById("problem-description").addEventListener("keydown", verifyPostLen);
-document.getElementById("problem-block").addEventListener("keydown", verifyBlockTyped);
-
-(function() {
-	document.getElementById("problem-title").focus();;
-})
-
-function isBlockValid(blockTyped) {
-	const re = /B{1,1}\d{1,1}/gi
-
-	return ((blockTyped.match(re)) ? true : false);
+	return (titleLength > titleMaxLength);
 }
 
-function verifyBlockTyped() {
-	const block		= document.getElementById("problem-block");
-	const blockTyped	= block.value;
+export function isBlockTypedValid(block) {
+	const correctBlockFormat = /B{1,1}\d{1,1}/g;
 
-	// i'll bullshit here!
-	if(!isBlockValid(blockTyped)) {
-		enableOrDisableBtn(block.value.length, 1, 2);
-	} else {
-		enableOrDisableBtn(block.value.length, 2, 2);
-	}
+	return correctBlockFormat.test(block);
 }
 
-function verifyTitleLength() {
-	const titleArea		= document.getElementById("problem-title");
-	const titleLength	= titleArea.value.length;
-
-	const titleMaxLen = 32;
-	const titleMinLen = calcMinFieldLen(titleMaxLen, 50);
-
-	enableOrDisableBtn(titleLength, titleMinLen, titleMaxLen);
-}
-
-function verifyPostLen() {
-	const postArea	= document.getElementById("problem-description");
-	const postLen	= postArea.value.length;
-
+export function isPostOverLimit(postLength) {
 	// We might need to resize it later...
 	const postMaxLen = 255;
-	const postMinLen = calcMinFieldLen(postMaxLen, 15);
 
-	enableOrDisableBtn(postLen, postMinLen, postMaxLen);
+	return (postLength > postMaxLen);
 }
