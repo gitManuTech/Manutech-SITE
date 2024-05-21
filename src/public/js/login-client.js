3fa451909cb020d30b744651857e2b16c70a9d36
Dante <truedevilhunterThe@gmail.com>
2024-05-21 15:40:06 -0300

    Repurposed login-client
    Now, instead of sending data to the server with AJAX, now
    it will just call "checkStudentData" and will cancel the submittion
    event if it finds "bad stuff" (for lack of better name)

--- src/public/js/login-client.js
+++ src/public/js/login-client.js
@@ -1,23 +1,34 @@
 import { getRawStudentData, getStudentData } from "../../private/js/student-data.js"
-import { createReq, mkReq } from "../../private/js/send-server.js";
-import { ELEMENTS_TO_SEND, SUBMIT_ERROR } from "../../private/js/enums.js";
+import { isNameValid, isClassroomValid, isCourseValid } from "./form-validation.js";
+import { SUBMIT_ERROR } from "../../private/js/enums.js"
 
-document.getElementById("student-form").addEventListener("submit", mkLoginReq, true);
+document.getElementById("student-form").addEventListener("submit", mkLoginCheck, true);
 
-function mkLoginReq(e) {
+function checkStudentData(name, classroom, course) {
+	if(isNameValid(name)) {
+		window.alert("Nome digitado acima do limite");
+		return SUBMIT_ERROR.PROBLEM_FORMAT;
+	}
+
+	if(!isClassroomValid(classroom)) {
+		window.alert("Sala inválida");
+		return SUBMIT_ERROR.PROBLEM_FORMAT;
+	}
+
+	if(isCourseValid(course)) {
+		window.alert("Curso digitado acima do limite");
+		return SUBMIT_ERROR.PROBLEM_FORMAT;
+	}
+
+	return true;
+}
+
+function mkLoginCheck(e) {
 	const rawStudentData	= getRawStudentData();
 	const studentData	= getStudentData(rawStudentData);
 
-	const requestData = createReq(rawStudentData, studentData, ELEMENTS_TO_SEND.ALL);
-	const requestPath = "../../private/php/login-server.php";
-
-	// I don't know if this is the best way to do it
-	if(requestData === SUBMIT_ERROR.PROBLEM_JSON) {
-		console.log(requestData);
-		console.log(studentData);
-	} else {
-		mkReq(requestData, requestPath, true);
+	// TODO: refactor
+	if(checkStudentData(studentData[0], studentData[1], studentData[2]) === SUBMIT_ERROR.PROBLEM_FORMAT) {
+		e.preventDefault();
 	}
-
-	e.preventDefault();
 }
