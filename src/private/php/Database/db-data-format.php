8c89cdb18e4b4e35eec0977d2e6aa5b8b9656fcd
Dante <truedevilhunterThe@gmail.com>
2024-05-26 12:28:01 -0300

    Changed how the post card will be generated

--- src/private/php/Database/db-data-format.php
+++ src/private/php/Database/db-data-format.php
@@ -48,14 +48,31 @@ END;
 
 function generate_post_profile(string $title, string $desc, string $block): void {
 echo "
-<div class=\"post-header-container\">
-	".generate_post_header($title, $desc, $block)."
-</div>
-<div class=\"post-desc\">
 
-	<p class=\"post-text\">".$desc."</p>
+<div class=\"post\">
+
+	<div class=\"post-header-container\">
+
+		<h2 class=\"post-header\">
+
+			<a href=\"student-post.php?ptitle={$title}&pblock={$block}&pdesc={$desc}\">
+
+				{$title}
+
+			</a>
+
+		</h2>
+
+	</div>
+
+	<div class=\"post-desc\">
+
+		<p class=\"post-text\">{$desc}</p>
+
+	</div>
 
 </div>";
+
 
 }
 
