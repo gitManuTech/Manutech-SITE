8338786c138089581c512de533bbf5652a085282
Dante <truedevilhunterThe@gmail.com>
2024-05-28 12:30:11 -0300

    Adding file to load "localStorage" content

---
+++ src/Front-end(Public)/js/load-student-data.js
@@ -0,0 +1,9 @@
+function loadStudentInfo() {
+	const infoPlaces = document.getElementsByClassName("student-info");
+
+	infoPlaces[0].innerText = localStorage["student-name"];
+	infoPlaces[1].innerText = localStorage["student-ra"];
+	infoPlaces[2].innerText = localStorage["student-course"];
+}
+
+
