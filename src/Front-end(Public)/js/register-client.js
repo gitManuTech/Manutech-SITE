218c1d8d3622c60bd149ae2157040367b6127c95
Dante <truedevilhunterThe@gmail.com>
2024-05-28 12:14:15 -0300

    Removing old code from register-client
    This is only temporary, but later on I'll have to add verification

--- src/Front-end(Public)/js/register-client.js
+++ src/Front-end(Public)/js/register-client.js
@@ -1,6 +1,5 @@
-import { getRawStudentData, getStudentData } from "../../private/js/student-data.js"
-import { createReq, mkReq } from "../../private/js/send-server.js";
-import { ELEMENTS_TO_SEND, SUBMIT_ERROR } from "../../private/js/enums.js";
+import { getRawStudentData, getStudentData storeLocally }
+	from "../../Back-end(Private)/js/student-data.js"
 
 document.getElementById("student-form").addEventListener("submit", mkRegisterReq, true);
 
@@ -8,15 +7,5 @@ function mkRegisterReq(e) {
 	const rawStudentData	= getRawStudentData();
 	const studentData	= getStudentData(rawStudentData);
 
-	const requestData	= createReq(rawStudentData, studentData, ELEMENTS_TO_SEND.ALL);
-	const requestPath	= "../../private/php/register-server.php";
-
-	// I don't know if this is the best way to do it
-	if(requestData === SUBMIT_ERROR.PROBLEM_FIELDS) {
-		e.preventDefault();
-	} else {
-		mkReq(requestData, requestPath, true);
-	}
-
-	e.preventDefault();
+	storeLocally(rawStudentData, studentData);
 }
