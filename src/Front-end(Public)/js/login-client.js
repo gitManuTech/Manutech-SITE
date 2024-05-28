22d71175ea44669b975270cec449bad00d7c0354
Dante <truedevilhunterThe@gmail.com>
2024-05-28 12:16:26 -0300

    Commenting stuff that will be necessary

--- src/Front-end(Public)/js/login-client.js
+++ src/Front-end(Public)/js/login-client.js
@@ -1,17 +1,28 @@
-import { getRawStudentData, getStudentData } from "../../private/js/student-data.js"
-import { isNameValid, isClassroomValid, isCourseValid } from "./form-validation.js";
-import { SUBMIT_ERROR } from "../../private/js/enums.js"
+import { getRawStudentData, getStudentData, storeLocally }
+	from "../../Back-end(Private)/js/student-data.js"
+import { isNameValid, isClassroomValid, isCourseValid }
+	from "./form-validation.js";
+import { SUBMIT_ERROR } from "../../Back-end(Private)/js/enums.js"
 
 document.getElementById("student-form").addEventListener("submit", mkLoginCheck, true);
 
-function checkStudentData(name, classroom, course) {
+function isRaValid(ra) {
+	console.log("TODO!");
+}
+
+function checkStudentData(name, ra, course) {
 	if(isNameValid(name)) {
 		window.alert("Nome digitado acima do limite");
 		return SUBMIT_ERROR.PROBLEM_FORMAT;
 	}
 
-	if(!isClassroomValid(classroom)) {
+	/*if(!isClassroomValid(classroom)) {
 		window.alert("Sala inválida");
+		return SUBMIT_ERROR.PROBLEM_FORMAT;
+	}*/
+
+	if(isRaValid(ra)) {
+		window.alert("RA digitado incorreto");
 		return SUBMIT_ERROR.PROBLEM_FORMAT;
 	}
 
@@ -28,10 +39,12 @@ function mkLoginCheck(e) {
 	const studentData	= getStudentData(rawStudentData);
 
 	const name	= studentData[0];
-	const room	= studentData[1];
+	const ra	= studentData[1];
 	const course	= studentData[2];
 
-	if(checkStudentData(name, room, course) === SUBMIT_ERROR.PROBLEM_FORMAT) {
+	/*if(checkStudentData(name, room, course) === SUBMIT_ERROR.PROBLEM_FORMAT) {
 		e.preventDefault();
-	}
+	}*/
+
+	storeLocally(rawStudentData, studentData);
 }
