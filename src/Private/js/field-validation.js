8a47634f80f1f5c2a08abf97e051a3824b434683
Dante <truedevilhunterThe@gmail.com>
2024-05-30 16:21:48 -0300

    Removing ENUM "ELEMENT_SEND"
    Because there is no need: we are not sending parts of student data,
    we are sending everything

--- src/Private/js/field-validation.js
+++ src/Private/js/field-validation.js
@@ -1,11 +1,3 @@
-export const ELEMENTS_TO_SEND = {
-	NONE: 0,
-	ONE: 1,
-	TWO: 2,
-	ALL: 3
-}
-
-/* Maybe separe this into a "enums-err-handling.js" file */
 export const SUBMIT_ERROR = {
 	PROBLEM_FIELDS: 0,
 	PROBLEM_FORMAT: 1,
