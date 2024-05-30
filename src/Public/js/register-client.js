7f34327c77866beba638303abcde4bd3596383c0
Dante <truedevilhunterThe@gmail.com>
2024-05-30 16:26:22 -0300

    Adding validation at client when submitting data
    Validation is being done at the client because we want the server
    not stressed

--- src/Public/js/register-client.js
+++ src/Public/js/register-client.js
@@ -1,11 +1,24 @@
 import { getRawStudentData, getStudentData, storeLocally }
 	from "../../Private/js/student-data.js";
 
+import { validateData } from "../../Private/js/validate-cred.js"
+
+import { SUBMIT_ERROR } from "../../Private/js/field-validation.js"
+
 document.getElementById("student-form").addEventListener("submit", mkRegisterReq, true);
 
 function mkRegisterReq(e) {
 	const rawStudentData	= getRawStudentData();
 	const studentData	= getStudentData(rawStudentData);
 
-	storeLocally(rawStudentData, studentData);
+	const name	= studentData[0];
+	const ra	= studentData[1];
+	const course	= studentData[2];
+
+	if(validateData(name, ra, course) === SUBMIT_ERROR.PROBLEM_FIELDS) {
+		window.alert("Ocorreu algum problema: verifique os campos digitados.");
+		e.preventDefault();
+	} else {
+		storeLocally(rawStudentData, studentData);
+	}
 }
