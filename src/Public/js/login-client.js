ec22e0d84f9a4a5d5cc73a2227d668be80f169d9
Dante <truedevilhunterThe@gmail.com>
2024-05-30 16:29:19 -0300

    Updating validation at "login-client"

--- src/Public/js/login-client.js
+++ src/Public/js/login-client.js
@@ -1,40 +1,11 @@
 import { getRawStudentData, getStudentData, storeLocally }
 	from "../../Private/js/student-data.js"
-import { isNameValid, isClassroomValid, isCourseValid }
-	from "./form-validation.js";
-import { SUBMIT_ERROR, SUBMIT_OK } from "../../Private/js/enums.js"
+
+import { validateData } from "../../Private/js/validate-cred.js";
+
+import { SUBMIT_ERROR } from "../../Private/js/field-validation.js"
 
 document.getElementById("student-form").addEventListener("submit", mkLoginCheck, true);
-
-function isRaValid(ra) {
-	console.log("TODO!");
-}
-
-function checkStudentData(name, ra, course) {
-	let data_status = SUBMIT_OK.FIELDS_OK;
-
-	if(isNameValid(name)) {
-		window.alert("Nome digitado acima do limite");
-		data_status = SUBMIT_ERROR.PROBLEM_FIELDS;
-	}
-
-	/*if(!isClassroomValid(classroom)) {
-		window.alert("Sala inválida");
-		return SUBMIT_ERROR.PROBLEM_FORMAT;
-	}*/
-
-	if(isRaValid(ra)) {
-		window.alert("RA digitado incorreto");
-		data_status = SUBMIT_ERROR.PROBLEM_FIELDS;
-	}
-
-	if(isCourseValid(course)) {
-		window.alert("Curso digitado acima do limite");
-		data_status = SUBMIT_ERROR.PROBLEM_FIELDS;
-	}
-
-	return data_status;
-}
 
 function mkLoginCheck(e) {
 	const rawStudentData	= getRawStudentData();
@@ -44,9 +15,9 @@ function mkLoginCheck(e) {
 	const ra	= studentData[1];
 	const course	= studentData[2];
 
-	/*if(checkStudentData(name, room, course) === SUBMIT_ERROR.PROBLEM_FORMAT) {
+	if(validateData(name, ra, course) === SUBMIT_ERROR.PROBLEM_FIELDS) {
 		e.preventDefault();
-	}*/
-
-	storeLocally(rawStudentData, studentData);
+	} else {
+		storeLocally(rawStudentData, studentData);
+	}
 }
