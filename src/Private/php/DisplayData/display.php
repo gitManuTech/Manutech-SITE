d4cb27ef8b23fb89af3654232eebcb429531e955
Dante <truedevilhunterThe@gmail.com>
2024-06-04 09:19:08 -0300

    Creating new dir and moving display.php to there

---
+++ src/Private/php/DisplayData/display.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once "Database/db-data-format.php";
+require_once "Database/db-student-queries.php";
+
+// TODO: combine this with display_posts() function
+function display_all_posts(): void {
+	$posts = select_all_student_posts();
+
+	while($post_data = $posts->fetch_assoc()) {
+		$title	= $post_data["Title"];
+		$desc	= $post_data["DSC"];
+		$block	= $post_data["Block"];
+		$author	= $post_data["Author"];
+
+		generate_post_home($title, $desc, $block, $author);
+	}
+}
+
+function display_posts(int $uid): void {
+	$posts = select_posts($uid);
+
+	while($post_data = $posts->fetch_assoc()) {
+		$title	= $post_data["Title"];
+		$desc	= $post_data["DSC"];
+		$block	= $post_data["Block"];
+
+		generate_post_profile($title, $desc, $block);
+	}
+}
+
+?>
