3f2e2be044afadbd10b75412056cfbaa11b8ab5b
Dante <truedevilhunterThe@gmail.com>
2024-06-11 10:24:02 -0300

    Updated account-config HTML structure and style

--- src/Private/php/Student/account-config.php
+++ src/Private/php/Student/account-config.php
@@ -1,7 +1,4 @@
-<?php
-// I need the $_SESSION["uid"], so i really need session_start() here
-session_start();
-?>
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 	<head>
@@ -12,38 +9,46 @@ session_start();
 		<link rel="stylesheet" type="text/css" href="../../../Public/css/general.css">
 		<link rel="stylesheet" type="text/css" href="../../../Public/css/header.css">
 		<link rel="stylesheet" type="text/css" href="../../../Public/css/acc-config.css">
+		<link rel="stylesheet" type="text/css" href="../../../Public/css/general-form.css">
 	</head>
 	<body onload="loadStudentInfo()">
 		<header class="header">
-			<h1 class="txt-color">
+			<h1 class="txt-color center">
 				Configurações de <span class="student-info"></span>
 			</h1>
 		</header>
 		<main class="main">
-			<form id="del-acc-form" name="del-acc-form" action="../ServerSide/del-acc-server.php" method="POST">
-				<div class="form-part">
-					<label for="del-acc-btn" class="del-acc">Deletar Conta:</label>
-					<button type="submit" id="del-acc-btn" name="del-acc-btn">Deletar</button>
-				</div>
-			</form>
-			<form id="ch-cour-form" name="ch-cour-form" autocomplete="off" onsubmit="return updateStudentCourse()">
-				<div class="form-part">
-					<label for="current-course">Curso atual:</label>
-					<input type="text" id="current-course" name="current-course"
-					value="<?php echo $_SESSION["scourse"]; ?>" readonly>
-					<label for="new-course">Curso novo:</label>
-					<input type="text" id="new-course" name="new-course"
-					placeholder="Nome do curso novo">
-				</div>
-				<div class="form-part">
-					<button type="submit" id="ch-cour-btn">Registrar curso atual</button>
-				</div>
-			</form>
+			<div id="account-modifs-container" class="student-form-container">
+				<form id="del-acc-form" 
+				name="del-acc-form" action="../ServerSide/del-acc-server.php" method="POST">
+					<div class="form-part">
+						<label for="del-acc-btn" class="txt-center txt-color">Deletar Conta</label>
+						<button type="submit" id="del-acc-btn" class="alter-acc-data"
+						name="del-acc-btn">Deletar</button>
+					</div>
+				</form>
+				<form id="ch-cour-form" name="ch-cour-form" autocomplete="off"
+				onsubmit="return updateStudentCourse()">
+					<div class="form-part">
+						<label for="current-course" class="txt-center txt-color">Curso atual</label>
+						<input type="text" id="current-course" name="current-course"
+						value="<?php echo $_SESSION["scourse"]; ?>" readonly>
+						<label for="new-course" class="txt-center txt-color">Curso novo</label>
+						<input type="text" id="new-course" name="new-course"
+						placeholder="Nome do curso novo">
+					</div>
+					<div class="form-part">
+						<button type="submit" id="ch-cour-btn" class="alter-acc-data">
+							Registrar curso atual
+						</button>
+					</div>
+				</form>
+			</div>
 		</main>
 		<footer class="footer">
-			<p>ManuTech group INC. &copy;2024</p>
+			<p class="txt-color">ManuTech group INC. &copy;2024</p>
 		</footer>
-		<script type="text/javascript" src="../../js/account-config.js"></script>
+		<script type="text/javascript" src="../../js/ch-course.js"></script>
 		<script type="text/javascript" src="../../js/load-student-data.js"></script>
 	</body>
 </html>
