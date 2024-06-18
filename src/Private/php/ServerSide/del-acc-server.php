373e8b5d4bf5577ea1d7a4c46f7c557d925403c8
Dante <truedevilhunterThe@gmail.com>
2024-06-18 16:09:59 -0300

    Reflecting updates of disable_student_acc

--- src/Private/php/ServerSide/del-acc-server.php
+++ src/Private/php/ServerSide/del-acc-server.php
@@ -4,7 +4,7 @@ session_start();
 
 require_once "../Database/db-queries.php";
 
-disable_student_acc($_SESSION["sname"], $_SESSION["sroom"], $_SESSION["scourse"]);
+disable_student_acc($_SESSION["sra"]);
 
 header("Location: ../../../Public/html/index.html");
 
