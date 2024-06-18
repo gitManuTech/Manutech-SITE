2674448b49e89ec8c6149f18ecf1b9831e31399d
Dante <truedevilhunterThe@gmail.com>
2024-06-18 16:51:19 -0300

    Fixing dumb stuff

--- src/Private/php/Database/db-connect.php
+++ src/Private/php/Database/db-connect.php
@@ -13,7 +13,7 @@ function connect_db(): mysqli {
     $db_name = "handyman";
     $db_host = "localhost";
     $db_user = "root";
-    $db_pass = "ghostlyTr1nk37";
+    $db_pass = "";
     
     $mysql = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
