87012f9023073e4014c0dea921486e63eed103f6
Dante <truedevilhunterThe@gmail.com>
2024-07-01 14:36:44 -0300

    Chnged ENUM name to java convention

--- src/Private/php/Enums/UserAccStat.php
+++ src/Private/php/Enums/UserAccStat.php
@@ -1,13 +1,11 @@
-<?php namespace Database\Enums;
+<?php namespace Enums\UserAccStat;
 
-// This ENUM should be treated as just number, since:
-// we are checking against the rows that are found in the DB
-enum STUDENT_ACC: int {
+enum StudentAcc: int {
 	case DISABLED = 0;
 	case ENABLED = 1;
 }
 
-enum STUDENT_STAT: int {
+enum StudentStat: int {
 	case HAS_NO_REG = 0;
 	case HAS_REG = 1;
 }
